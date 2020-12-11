ad871de4f3ee553e18e155f70d0c98cb8a37ad9d
Matthew Kirby <matt.kirby@yahoo.com>
2020-12-11 13:35:29 -0500

    updated campaign controller

--- app/controllers/api/v1/campaigns_controller.rb
+++ app/controllers/api/v1/campaigns_controller.rb
@@ -1,17 +1,43 @@
 class Api::V1::CampaignsController < ApplicationController
+  before_action :find_campaign, only: [:update, :destroy]
+
+  def index
+    # FIXME dont like how this is returning without hitting the serializer but haven't quite figured it out yet
+    campaigns = []
+    current_user.campaigns.each{ |i| campaigns.push(i)}
+    render json: { campaigns: campaigns}, status: :created
+  end
 
   def create
     campaign = Campaign.create(campaign_params)
     if campaign.valid?
-      # create the user camapaign here and set true for owner
       user_campaign = UserCampaign.create(user_id: current_user.id, campaign_id: campaign.id, owner: true)
-      render json: { campaign: CampaignSerializer.new(campaign)}, status: :created
+      if user_campaign.valid?
+        render json: { campaign: CampaignSerializer.new(campaign)}, status: :created
+      else
+        ender json: { error: 'failed to link user to a campaign' }, status: :not_acceptable
+      end
     else
       render json: { error: 'failed to create campaign' }, status: :not_acceptable
     end
   end
 
+  def update
+    @campaign.update(campaign_params)
+    render json: { campaign: CampaignSerializer.new(@campaign) }, status: :ok
+  end
+
+  def destroy
+    # TODO gonna need to decide how much to delete when a campaign is deleted
+    @campaign.delete
+    render json: {}, status: :accepted
+  end
+
   private
+  
+  def find_campaign
+    @campaign = Campaign.find(params[:id])
+  end
 
   def campaign_params
     params.require(:campaign).permit(:name)
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :users, only: [:create]
-      resources :campaigns, only: [:create]
+      resources :campaigns, only: [:index, :create, :update]
       post '/login', to: 'auth#create'
       get '/profile', to: 'users#profile'
     end
