8030bf2cbd5a5a39e2c324a9c6cab2d10a5b8a65
Matthew Kirby <matt.kirby@yahoo.com>
2020-12-14 21:35:51 -0500

    added players to campaign serializer

--- app/serializers/campaign_serializer.rb
+++ app/serializers/campaign_serializer.rb
@@ -1,5 +1,11 @@
 class CampaignSerializer < ActiveModel::Serializer
-  attributes :id, :name, :parties, :characters, :created_at
+  attributes :id, :name, :parties, :characters, :players, :created_at
   has_many :parties
   has_many :characters, through: :parties
+
+  def players
+    users = []
+    object.users.each { |user| users.push({id: user.id, username: user.username, avatar: user.avatar })}
+    users
+  end
 end
