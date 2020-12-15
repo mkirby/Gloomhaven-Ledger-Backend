81cdace76a1aed6771f525514c49f8dbb16b1d05
Matthew Kirby <matt.kirby@yahoo.com>
2020-12-15 12:52:44 -0500

    added campaign details to character serializer

--- app/serializers/character_serializer.rb
+++ app/serializers/character_serializer.rb
@@ -1,5 +1,9 @@
 class CharacterSerializer < ActiveModel::Serializer
-  attributes :id, :name, :level, :experience, :gold, :checkmarks, :active, :retired, :party
+  attributes :id, :name, :level, :experience, :gold, :checkmarks, :active, :retired, :party, :campaign
   belongs_to :party
   belongs_to :user
+  
+  def campaign
+    object.party.campaign
+  end
 end
