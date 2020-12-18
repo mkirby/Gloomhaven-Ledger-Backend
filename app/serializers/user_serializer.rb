7ba25ee886bd2ece624c5967748267d3d5b20171
Matthew Kirby <matt.kirby@yahoo.com>
2020-12-18 13:42:50 -0500

    refactored user_serializer to not pass user_campaigns to the frontend

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,15 +1,6 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :username, :avatar, :parties
-  has_many :user_campaigns
   has_many :campaigns, through: :user_campaigns
   has_many :characters
   has_many :parties, through: :characters
-
-  # def parties
-  #   user_parties = []
-  #   user = User.find(object.id)
-  #   unique_user_parties = user.parties.uniq
-  #   unique_user_parties.each { |party| user_parties.push(PartySerializer.new(party))}
-  #   user_parties
-  # end
 end
