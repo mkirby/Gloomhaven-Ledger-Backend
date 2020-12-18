683520bcf1d9b4ba2384934227f01a748f40edc1
Matthew Kirby <matt.kirby@yahoo.com>
2020-12-18 13:18:55 -0500

    added an owner method to the campaign serializer

--- app/serializers/campaign_serializer.rb
+++ app/serializers/campaign_serializer.rb
@@ -1,5 +1,5 @@
 class CampaignSerializer < ActiveModel::Serializer
-  attributes :id, :name, :parties, :characters, :players, :created_at
+  attributes :id, :name, :parties, :characters, :players, :owner, :created_at
   has_many :parties
   has_many :characters, through: :parties
 
@@ -7,5 +7,15 @@ class CampaignSerializer < ActiveModel::Serializer
     users = []
     object.users.each { |user| users.push({id: user.id, username: user.username, avatar: user.avatar })}
     users
+  end
+
+  def owner
+    user_campaign_owner = object.user_campaigns.find { |uc| uc.owner}
+    owner = User.find_by(id: user_campaign_owner.user_id)
+    ownerHash = {
+      id: owner.id,
+      username: owner.username,
+      avatar: owner.avatar
+    }
   end
 end
