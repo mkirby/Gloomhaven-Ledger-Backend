bde811c911c8997ed5c2ef28281c8bef963bee56
Matthew Kirby <matt.kirby@yahoo.com>
2021-01-02 15:57:23 -0500

    added notes to character params and the character serializer

--- app/serializers/character_serializer.rb
+++ app/serializers/character_serializer.rb
@@ -1,5 +1,5 @@
 class CharacterSerializer < ActiveModel::Serializer
-  attributes :id, :name, :level, :experience, :gold, :checkmarks, :active, :retired, :character_class, :party, :campaign
+  attributes :id, :name, :level, :experience, :gold, :checkmarks, :active, :retired, :notes, :character_class, :party, :campaign
   belongs_to :party
   belongs_to :user
   belongs_to :character_class
